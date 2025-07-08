18c7cf5fa806943f114753f613831d59f67bdd3f
DanaHafez <dana7afez@gmail.com>
2025-07-08 18:44:14 +0300

    11

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === "production" ? "/solutions_by_stc" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/solutions_by_stc" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/DNS_Solutions_by_stc" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/DNS_Solutions_by_stc/" : "",
 }
 
 module.exports = nextConfig
